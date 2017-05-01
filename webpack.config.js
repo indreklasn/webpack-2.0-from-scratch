c4978f9491793fc755b261b031125b5cc5dc1387
Indrek Lasn <lasnindrek@gmail.com>
2017-05-01 14:32:57 +0300

    chapter 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 let config = {
-  entry: './index.js',
+
+  entry: './src/index.js', // entry file
   output: {
-    filename: 'output.js'
+    path: path.resolve(__dirname, './public'), // ouput path
+    filename: 'output.js' // output filename
   },
   module: {
     rules: [
@@ -14,10 +18,23 @@ let config = {
       },
       {
         test: /\.scss$/, // files ending with .scss
-        loader: ['style-loader', 'css-loader', 'sass-loader'] // use these loaders
+        use: ExtractTextWebpackPlugin.extract({ // call our plugin with extract method
+          use: ['css-loader', 'sass-loader'], // use these loaders
+          fallback: 'style-loader' // fallback for any CSS not extracted
+        }) // end extract
       }
-    ]
-  }
+    ] // end rules
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin('styles.css') // call the ExtractTextWebpackPlugin constructor and name our css file
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'), // A directory or URL to serve HTML content from.
+    historyApiFallback: true, // fallback to /index.html for Single Page Applications.
+    inline: true, // inline mode (set to false to disable including client scripts (like livereload)
+    open: true // open default browser while launching
+  },
+  devtool: 'eval-source-map' // enable devtool for better debugging experience
 }
 
 module.exports = config;
