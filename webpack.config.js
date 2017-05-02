91e3d6dcacc35e5b573ddfebf7c8eded5fb2f52a
Indrek Lasn <lasnindrek@gmail.com>
2017-05-02 17:21:28 +0300

    chapter 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack'); // webpack itself
+const path = require('path'); // nodejs dependency when dealing with paths
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'); // require webpack plugin
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); // require webpack plugin
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin'); // require webpack plugin
 
-let config = {
+let config = { // config object
 
   entry: './src/index.js', // entry file
-  output: {
+  output: { // output
     path: path.resolve(__dirname, './public'), // ouput path
     filename: 'output.js' // output filename
   },
   module: {
-    rules: [
+    rules: [ // loader rules
       {
         test: /\.js$/, // files ending with .js
         exclude: /node_modules/, // exclude the node_modules directory
-        loader: "babel-loader" // use this (babel-core) loader
+        loader: 'babel-loader' // use this (babel-core) loader
       },
       {
         test: /\.scss$/, // files ending with .scss
@@ -25,8 +27,8 @@ let config = {
       }
     ] // end rules
   },
-  plugins: [
-    new ExtractTextWebpackPlugin('styles.css') // call the ExtractTextWebpackPlugin constructor and name our css file
+  plugins: [ // webpack plugins
+    new ExtractTextWebpackPlugin('styles.css'), // call the ExtractTextWebpackPlugin constructor and name our css file
   ],
   devServer: {
     contentBase: path.resolve(__dirname, './public'), // A directory or URL to serve HTML content from.
@@ -34,7 +36,14 @@ let config = {
     inline: true, // inline mode (set to false to disable including client scripts (like livereload)
     open: true // open default browser while launching
   },
-  devtool: 'eval-source-map' // enable devtool for better debugging experience
+  devtool: 'eval-source-map', // enable devtool for better debugging experience
 }
 
 module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin(), // call the uglify plugin
+    new OptimizeCSSAssets() // call the css optimizer (minfication)
+  );
+}
